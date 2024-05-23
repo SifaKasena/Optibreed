22390df79fd562d804474c4b9a6480a5c84c0d3f
Rotich Kibet <asbelkibetrotich@gmail.com>
2024-05-23 12:04:01 +0300

    Room management (add and view)

--- attachment/optibreed/urls.py
+++ attachment/optibreed/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path("signup/", views.SignupView.as_view(), name="signup"),
     path("login/", auth_views.LoginView.as_view(), name="login"),
     path("home/", views.home, name="home"),
-    # path("rooms/new/", views.new, name="new"),
-    # path("rooms/<int:room_id>/", views.detail, name="detail"),
+    path('add_room/', views.add_room, name='add_room'),
+    path("rooms/<int:room_id>/", views.detail, name="rooms"),
 ]
