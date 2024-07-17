f2a768d81d18f608b253fdda092686df59442c92
MoseNjau <moses.njau@students.jkuat.ac.ke>
2024-07-17 13:35:43 +0300

    index styling

--- attachment/attachment/settings.py
+++ attachment/attachment/settings.py
@@ -130,7 +130,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/dashboard/'
-LOGOUT_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/index'
 
 # Then set the SECURE_REFERRER_POLICY
 SECURE_REFERRER_POLICY = 'no-referrer-when-downgrade'
